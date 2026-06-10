c76cdfe8f13aaf3111fd3cc0a9ea7ebda93c1f88
timotheosf <timoteofassoni@gmail.com>
2026-06-10 08:21:04 -0300

    Ilimited polymorfism added for receive arbitrary numerical type data

--- src/kinds.f90
+++ src/kinds.f90
@@ -13,17 +13,17 @@ module kinds_mod
     implicit none
     
     type :: random_data
-        class(*) , allocatable :: arr( : ) !> Polymorphic variable
-        logical :: sorted_data
-        integer(i4) :: length
-        real(dp) :: real_length
-        
-
+        !> arr express, in real double precision, any type of numerical data
+        real(dp) , allocatable :: arr( : )
+        integer(i4) :: len      !> Integer type data length for loops
+        real(dp) :: real_len    !> Real type data length for calculations
+        !> Flag to diferentiate between discrete and continuous methods for MLE
+        logical :: data_is_discrete
+        !> Flag to save in the memory if data is already sorted
+        logical :: sorted_data  
     contains
-        
         !> Procedure for receiving data
-        procedure :: receive_data
-        
+        procedure :: receive_data        
         !> Procedure for sorting data
         procedure :: sort_data => sorting_random_data
     end type
@@ -31,88 +31,65 @@ module kinds_mod
 contains
 
 subroutine receive_data( this , r_data )
+    !> This subroutine can recive any numerical data type (except qp), and
+    !   converts the data to real double precision, to simply the treatment
+    !   However, as a good pratice, we've manteined the "wraps" to the power law distribution, if one wants to evaluete pdf/cdf/ccdf at any numerical type value
     class(random_data) , intent(inout) :: this
-    class(*) , intent(in) :: r_data(:)
+    class(*) , intent(in) :: r_data(:) !> Polymorphic variable
 
     if (allocated(this%arr)) deallocate(this%arr)
-    
+    allocate( this%arr(size(r_data)) )
     select type( r_data )
         type is ( real(sp) )
-            allocate( this%arr , source=r_data )
+            this%data_is_discrete = .FALSE.
+            this%arr = real(r_data, dp)
         type is ( real(dp) )
-            allocate( this%arr , source=r_data )
+            this%data_is_discrete = .FALSE.
+            this%arr = r_data
         type is ( integer(i1) )
-            allocate( this%arr , source=r_data )
+            this%data_is_discrete = .TRUE.
+            this%arr = real(r_data, dp)
         type is ( integer(i2) )
-            allocate( this%arr , source=r_data )
+            this%data_is_discrete = .TRUE.
+            this%arr = real(r_data, dp)
         type is ( integer(i4) )
-            allocate( this%arr , source=r_data )
+            this%data_is_discrete = .TRUE.
+            this%arr = real(r_data, dp)
         type is ( integer(i8) )
-            allocate( this%arr , source=r_data )
+            this%data_is_discrete = .TRUE.
+            this%arr = real(r_data, dp)
         class default
             !> If generic_array is a string or an unsupported type, print an error message
             print *, "Error: data type not supported."
         end select
+
+        this%sorted_data = .FALSE.
 end subroutine
 
 subroutine sorting_random_data( this , pre_ordering , reverse )
+    !> This subroutines plays the role of an interface to ordering the data
     class(random_data) , intent(inout) :: this
     logical, intent(in), optional :: pre_ordering, reverse
     logical :: is_arr_pre_ordering, ordering_in_reverse
 
     !> Set default values for optional arguments
-    is_arr_pre_ordering = .false.
+    is_arr_pre_ordering = .FALSE.
     if (present(pre_ordering)) is_arr_pre_ordering = pre_ordering
-    ordering_in_reverse = .false.
+    ordering_in_reverse = .FALSE.
     if (present(reverse)) ordering_in_reverse = reverse
 
     !> Check if the array is allocated
     if (.not. allocated(this%arr)) return
 
-    select type( generic_array => this%arr )
-        type is ( real(sp) )
-            if (is_arr_pre_ordering) then
-                call ord_sort( generic_array , reverse=ordering_in_reverse)
-            else
-                call radix_sort( generic_array , reverse=ordering_in_reverse)
-           endif
-        type is ( real(dp) )
-            if (is_arr_pre_ordering) then
-                call ord_sort( generic_array , reverse=ordering_in_reverse)
-            else
-                call radix_sort( generic_array , reverse=ordering_in_reverse)
-           endif
-        type is ( integer(i1) )
-            if (is_arr_pre_ordering) then
-                call ord_sort( generic_array , reverse=ordering_in_reverse)
-            else
-                call radix_sort( generic_array , reverse=ordering_in_reverse)
-           endif
-        type is ( integer(i2) )
-            if (is_arr_pre_ordering) then
-                call ord_sort( generic_array , reverse=ordering_in_reverse)
-            else
-                call radix_sort( generic_array , reverse=ordering_in_reverse)
-           endif
-        type is ( integer(i4) )
-            if (is_arr_pre_ordering) then
-                call ord_sort( generic_array , reverse=ordering_in_reverse)
-            else
-                call radix_sort( generic_array , reverse=ordering_in_reverse)
-           endif
-        type is ( integer(i8) )
-            if (is_arr_pre_ordering) then
-                call ord_sort( generic_array , reverse=ordering_in_reverse)
-            else
-                call radix_sort( generic_array , reverse=ordering_in_reverse)
-           endif
-        class default
-            !> If generic_array is a string or an unsupported type, print an error message
-            print *, "Error: data type not supported."
-        end select
-        this%sorted_data = .true.
-        this%length = size( this%arr )
-        this%real_length = real( size( this%arr ) , dp )
+    if (is_arr_pre_ordering) then
+        call ord_sort( this%arr , reverse=ordering_in_reverse)
+    else
+        call radix_sort( this%arr , reverse=ordering_in_reverse)
+    endif
+    
+    this%sorted_data = .TRUE.
+    this%len = size( this%arr )
+    this%real_len = real( size( this%arr ) , dp )
 end subroutine
 
 end module kinds_mod    
