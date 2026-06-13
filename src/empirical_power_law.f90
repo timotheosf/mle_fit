159a3d16213ffe876d340ee26be882ca0e1f474d
timotheosf <timoteo.fassoni@ufv.br>
2026-06-13 20:49:11 -0300

    p_valuing after greed fitting

--- src/empirical_power_law.f90
+++ src/empirical_power_law.f90
@@ -134,7 +134,7 @@ subroutine find_greed_parameters_at_all_cost( this , r_data , greed_xmin , greed
             endif
         endif
     enddo find_greed_parameteres
-
+    call this%p_value( N_samples=2500 )
 end subroutine find_greed_parameters_at_all_cost
 
 
